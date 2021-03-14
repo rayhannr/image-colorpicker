d2d079e913b44edabd999dbbb4a7d753124fd92f
rayhannr <rayhannaufal2017@mail.ugm.ac.id>
2021-03-14 13:22:05 +0700

    init

---
+++ postcss.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    plugins: [
+        require('tailwindcss'),
+        require('autoprefixer'),
+        // require('@fullhuman/postcss-purgecss')({
+        //     content: [
+        //         './src/**/*.tsx',
+        //         './src/**/*.ts',
+        //         './public/index.html'
+        //       ],
+        //       css: ['./src/tailwind.css'],
+        //     defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
+        // })
+    ]
+}
