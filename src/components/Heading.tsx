0da09abd2014ae59a78ea3eb525efbc64773bd60
rayhannr <rayhannaufal2017@mail.ugm.ac.id>
2021-03-15 17:52:08 +0700

    update text in heading

--- src/components/Heading.tsx
+++ src/components/Heading.tsx
@@ -6,8 +6,8 @@ const Heading: React.FC = () => (
             Image Color Picker
             </h1>
         <h2 className="font-medium text-lg text-center text-gray-500 mt-2 w-4/5 sm:w-auto mx-auto">Pick a color from your image easily</h2>
-        <h3 className="text-center text-sm xl:text-base text-gray-400 mt-1 mb-5 mx-auto w-full md:w-10/12">
-            Upload an image from your device, click any pixel on the image to get its color once it gets loaded. <br /> We also show you the closest <a className="text-light-blue-600 font-medium hover:text-gray-700" href="https://tailwindcss.com/docs/customizing-colors" target="_blank" rel="noreferrer">Tailwind color</a> of your picked color.
+        <h3 className="text-center text-sm xl:text-base text-gray-400 mt-1 mb-5 mx-auto w-full md:w-10/12 lg:w-8/12 xl:w-1/2">
+            Upload an image from your device, click any pixel on the image to get its color and the closest <a className="text-light-blue-600 font-medium hover:text-gray-700" href="https://tailwindcss.com/docs/customizing-colors" target="_blank" rel="noreferrer">Tailwind color</a> once it gets loaded.
         </h3>
     </>
 )
