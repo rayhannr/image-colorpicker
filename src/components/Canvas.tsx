468b14cbea22240b42c04d22e37974a82da755e7
rayhannr <rayhannr07@gmail.com>
2024-04-24 21:22:33 +0700

    fix: draw canvas on image load

--- src/components/Canvas.tsx
+++ src/components/Canvas.tsx
@@ -3,69 +3,76 @@ import React, { useEffect, useRef } from 'react'
 import { RGB } from '../constants/colors'
 
 interface Props {
-    imageUrl: string
-    container: React.RefObject<HTMLDivElement>
-    onPickColor: (color: RGB) => void
+  imageUrl: string
+  container: React.RefObject<HTMLDivElement>
+  onPickColor: (color: RGB) => void
 }
 
-const Canvas: React.FC<Props> = props => {
-    const canvasRef = useRef<HTMLCanvasElement>(null)
-    const imageRef = useRef<HTMLImageElement>(null)
+const Canvas: React.FC<Props> = (props) => {
+  const canvasRef = useRef<HTMLCanvasElement>(null)
+  const imageRef = useRef<HTMLImageElement>(null)
 
-    const { imageUrl, container } = props
+  const { imageUrl, container } = props
 
-    useEffect(() => {
-        const drawCanvas = () => {
-            const canvas = canvasRef.current
-            if(canvas){
-                const context: CanvasRenderingContext2D = canvas.getContext('2d')!
-                
-                const image: HTMLImageElement = imageRef.current!
-                const aspectRatio: number = image.width / image.height
-                const containerWidth: number = container.current!.getBoundingClientRect().width
-                
-                canvas.width = Math.min(image.width, containerWidth)
-                canvas.height = Math.min(image.height, containerWidth / aspectRatio)
-                
-                if (canvas.height > window.innerHeight) {
-                    canvas.height = window.innerHeight - 100
-                    canvas.width = canvas.height * aspectRatio
-                }
-                context.drawImage(image, 0, 0, canvas.width, canvas.height)
-            }
-        }
-        if (imageUrl) {
-            drawCanvas()
-            window.addEventListener('resize', () => {
-                drawCanvas()
-            })
-        }
-    }, [imageUrl, container])
+  const drawCanvas = () => {
+    const canvas = canvasRef.current
+    if (canvas) {
+      const context: CanvasRenderingContext2D = canvas.getContext('2d')!
 
-    const pickColor = (event: any) => {
-        if (imageUrl) {
-            const canvas: HTMLCanvasElement = canvasRef.current!
-            const context: CanvasRenderingContext2D = canvas.getContext('2d')!
+      const image: HTMLImageElement = imageRef.current!
+      const aspectRatio: number = image.width / image.height
+      const containerWidth: number = container.current!.getBoundingClientRect().width
 
-            const canvasBounding = canvas.getBoundingClientRect()
-            const xCoord: number = Math.round(event.clientX - canvasBounding.left)
-            const yCoord: number = Math.round(event.clientY - canvasBounding.top)
+      canvas.width = Math.min(image.width, containerWidth)
+      canvas.height = Math.min(image.height, containerWidth / aspectRatio)
 
-            const pickedColor: Uint8ClampedArray = context.getImageData(xCoord, yCoord, 1, 1).data
-            const red: number = pickedColor[0]
-            const green: number = pickedColor[1]
-            const blue: number = pickedColor[2]
-            const rgb: RGB = [red, green, blue]
-            props.onPickColor(rgb)
-        }
+      if (canvas.height > window.innerHeight) {
+        canvas.height = window.innerHeight - 100
+        canvas.width = canvas.height * aspectRatio
+      }
+      context.drawImage(image, 0, 0, canvas.width, canvas.height)
     }
+  }
 
-    return (
-        <>
-            <canvas ref={canvasRef} className="mx-auto pb-10" style={{ cursor: imageUrl ? 'crosshair' : 'auto' }} onClick={pickColor}></canvas>
-            <img src={props.imageUrl} className="hidden" ref={imageRef} alt="" />
-        </>
-    )
+  useEffect(() => {
+    if (!imageUrl) return
+
+    window.addEventListener('resize', drawCanvas)
+
+    return () => {
+      window.removeEventListener('resize', drawCanvas)
+    }
+  }, [imageUrl, container])
+
+  const pickColor = (event: any) => {
+    if (imageUrl) {
+      const canvas: HTMLCanvasElement = canvasRef.current!
+      const context: CanvasRenderingContext2D = canvas.getContext('2d')!
+
+      const canvasBounding = canvas.getBoundingClientRect()
+      const xCoord: number = Math.round(event.clientX - canvasBounding.left)
+      const yCoord: number = Math.round(event.clientY - canvasBounding.top)
+
+      const pickedColor: Uint8ClampedArray = context.getImageData(xCoord, yCoord, 1, 1).data
+      const red: number = pickedColor[0]
+      const green: number = pickedColor[1]
+      const blue: number = pickedColor[2]
+      const rgb: RGB = [red, green, blue]
+      props.onPickColor(rgb)
+    }
+  }
+
+  return (
+    <>
+      <canvas
+        ref={canvasRef}
+        className="mx-auto pb-10"
+        style={{ cursor: imageUrl ? 'crosshair' : 'auto' }}
+        onClick={pickColor}
+      ></canvas>
+      <img src={props.imageUrl} className="hidden" ref={imageRef} onLoad={drawCanvas} alt="" />
+    </>
+  )
 }
 
 export default Canvas
